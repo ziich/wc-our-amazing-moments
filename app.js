9c76ac90812941504987cb348e7f9000e0617d48
yunshengji <423450928@qq.com>
2018-08-06 16:11:50 +0800

    initial commit add the login

---
+++ app.js
@@ -0,0 +1,27 @@
+App({
+  onLaunch: function(){
+    const host = 'http://localhost:3000/'
+    console.log('processing to login')
+    wx.login({
+      success:res=>{
+        console.log(res)
+        
+        wx.request({
+          url: host + 'login',
+          method: 'post',
+          data: {
+            code: res.code
+          },
+          success:res =>{
+            console.log(res)
+            this.globalData.userId = res.data.userId
+          }
+        })
+      }
+    })
+  },
+  
+  globalData: {
+
+  }
+})
